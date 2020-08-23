9746923e4367b9f5a3c92c50a05f16808fe2584e
James Leong <jameszleong@gmail.com>
2020-08-23 12:11:11 +0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ James Leong 2020</p>
+    </footer>
+  );
+}
+
+export default Footer;
